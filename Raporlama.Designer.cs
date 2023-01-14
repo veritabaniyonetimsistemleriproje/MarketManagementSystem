b7d358983f7c6d1206003d0a88518ce1ea7e8ddb
beril töman <beriltoman@outlook.com>
2023-01-14 19:47:04 +0300

    Raporlama arayüzü tasarımı oluşturuldu. Altı adet raporlama formu oluşturuldu. Butonlarla gönderim sağlandı.

--- Raporlama.Designer.cs
+++ Raporlama.Designer.cs
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtnKarZararUrun = new System.Windows.Forms.Button();
+            this.BtnTopluMusBorc = new System.Windows.Forms.Button();
+            this.BtnMusBazlıRapor = new System.Windows.Forms.Button();
+            this.BtnKarZarar = new System.Windows.Forms.Button();
+            this.BtnSatısTrendi = new System.Windows.Forms.Button();
+            this.BtnCokSatandanAza = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnKarZararUrun
+            // 
+            this.BtnKarZararUrun.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnKarZararUrun.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnKarZararUrun.Location = new System.Drawing.Point(10, 13);
+            this.BtnKarZararUrun.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnKarZararUrun.Name = "BtnKarZararUrun";
+            this.BtnKarZararUrun.Size = new System.Drawing.Size(217, 116);
+            this.BtnKarZararUrun.TabIndex = 0;
+            this.BtnKarZararUrun.Text = "Ürün Bazlı Kar Zarar Durum Raporu";
+            this.BtnKarZararUrun.UseVisualStyleBackColor = false;
+            this.BtnKarZararUrun.Click += new System.EventHandler(this.BtnKarZararUrun_Click);
+            // 
+            // BtnTopluMusBorc
+            // 
+            this.BtnTopluMusBorc.BackColor = System.Drawing.Color.LawnGreen;
+            this.BtnTopluMusBorc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnTopluMusBorc.Location = new System.Drawing.Point(330, 13);
+            this.BtnTopluMusBorc.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnTopluMusBorc.Name = "BtnTopluMusBorc";
+            this.BtnTopluMusBorc.Size = new System.Drawing.Size(217, 116);
+            this.BtnTopluMusBorc.TabIndex = 1;
+            this.BtnTopluMusBorc.Text = "Toplu Müşteri Borç Durum Raporu";
+            this.BtnTopluMusBorc.UseVisualStyleBackColor = false;
+            this.BtnTopluMusBorc.Click += new System.EventHandler(this.BtnTopluMusBorc_Click);
+            // 
+            // BtnMusBazlıRapor
+            // 
+            this.BtnMusBazlıRapor.BackColor = System.Drawing.Color.LawnGreen;
+            this.BtnMusBazlıRapor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnMusBazlıRapor.Location = new System.Drawing.Point(657, 13);
+            this.BtnMusBazlıRapor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnMusBazlıRapor.Name = "BtnMusBazlıRapor";
+            this.BtnMusBazlıRapor.Size = new System.Drawing.Size(217, 116);
+            this.BtnMusBazlıRapor.TabIndex = 2;
+            this.BtnMusBazlıRapor.Text = "Müşteri Bazlı Rapor";
+            this.BtnMusBazlıRapor.UseVisualStyleBackColor = false;
+            this.BtnMusBazlıRapor.Click += new System.EventHandler(this.BtnMusBazlıRapor_Click);
+            // 
+            // BtnKarZarar
+            // 
+            this.BtnKarZarar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnKarZarar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnKarZarar.Location = new System.Drawing.Point(10, 242);
+            this.BtnKarZarar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnKarZarar.Name = "BtnKarZarar";
+            this.BtnKarZarar.Size = new System.Drawing.Size(217, 113);
+            this.BtnKarZarar.TabIndex = 3;
+            this.BtnKarZarar.Text = "Market Kar Zarar Durumu";
+            this.BtnKarZarar.UseVisualStyleBackColor = false;
+            this.BtnKarZarar.Click += new System.EventHandler(this.BtnKarZarar_Click);
+            // 
+            // BtnSatısTrendi
+            // 
+            this.BtnSatısTrendi.BackColor = System.Drawing.Color.Yellow;
+            this.BtnSatısTrendi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnSatısTrendi.Location = new System.Drawing.Point(330, 242);
+            this.BtnSatısTrendi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnSatısTrendi.Name = "BtnSatısTrendi";
+            this.BtnSatısTrendi.Size = new System.Drawing.Size(217, 113);
+            this.BtnSatısTrendi.TabIndex = 4;
+            this.BtnSatısTrendi.Text = "İki Tarih Arası Satış Trendi";
+            this.BtnSatısTrendi.UseVisualStyleBackColor = false;
+            this.BtnSatısTrendi.Click += new System.EventHandler(this.BtnSatısTrendi_Click);
+            // 
+            // BtnCokSatandanAza
+            // 
+            this.BtnCokSatandanAza.BackColor = System.Drawing.Color.Yellow;
+            this.BtnCokSatandanAza.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnCokSatandanAza.Location = new System.Drawing.Point(657, 242);
+            this.BtnCokSatandanAza.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnCokSatandanAza.Name = "BtnCokSatandanAza";
+            this.BtnCokSatandanAza.Size = new System.Drawing.Size(217, 113);
+            this.BtnCokSatandanAza.TabIndex = 5;
+            this.BtnCokSatandanAza.Text = "Ürün Listesi (çok satandan az satana)";
+            this.BtnCokSatandanAza.UseVisualStyleBackColor = false;
+            this.BtnCokSatandanAza.Click += new System.EventHandler(this.BtnCokSatandanAza_Click);
+            // 
+            // Raporlama
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(890, 394);
+            this.Controls.Add(this.BtnCokSatandanAza);
+            this.Controls.Add(this.BtnSatısTrendi);
+            this.Controls.Add(this.BtnKarZarar);
+            this.Controls.Add(this.BtnMusBazlıRapor);
+            this.Controls.Add(this.BtnTopluMusBorc);
+            this.Controls.Add(this.BtnKarZararUrun);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Raporlama";
             this.Text = "Raporlama";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnKarZararUrun;
+        private System.Windows.Forms.Button BtnTopluMusBorc;
+        private System.Windows.Forms.Button BtnMusBazlıRapor;
+        private System.Windows.Forms.Button BtnKarZarar;
+        private System.Windows.Forms.Button BtnSatısTrendi;
+        private System.Windows.Forms.Button BtnCokSatandanAza;
     }
 }
