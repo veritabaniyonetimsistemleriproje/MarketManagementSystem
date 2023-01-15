d8c57dcadbdb369244c0f13468c68730639bea9a
beril töman <beriltoman@outlook.com>
2023-01-16 00:16:15 +0300

    Raporlama kısmında ürün listesi çoktan aza kısmı yapıldı

--- Raporlama Formları/UrunListCoktanAza.cs
+++ Raporlama Formları/UrunListCoktanAza.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,47 @@ namespace MarketManagementSystem
         public UrunListCoktanAza()
         {
             InitializeComponent();
+        }
+        MarketManagementSystemEntities1 db = new MarketManagementSystemEntities1();
+        DataTable dt = new DataTable();
+
+        private void UrunListCoktanAza_Load(object sender, EventArgs e)
+        {
+     
+            try
+            {
+                
+                    var sorgu = from sepetUrun in db.SepetUruns
+                                group sepetUrun by sepetUrun.urunBarkod into s
+                                select new
+                                {
+                                    barkod = s.Key,
+                                    miktar = s.Sum(x => x.satisMiktar),
+                                };
+                    var deneme = from urun in db.Uruns
+                                 select new
+                                 {
+                                   
+                                     barkod = urun.urunBarkod,
+                                     ad = urun.urunAd,
+                                 };
+
+                var q = from sor in sorgu
+                        from dene in deneme
+                        where sor.barkod == dene.barkod
+                        select new
+                        {
+                            Ad = dene.ad,
+                            Barkod = dene.barkod,
+                            ToplamSatış = sor.miktar,
+                        };
+                DGVListelemeUrun.DataSource = q.OrderByDescending(x => x.ToplamSatış).ToList();
+
+            }
+            catch
+            {
+                MessageBox.Show("Lütfen kontrol edin");
+            }
         }
     }
 }
