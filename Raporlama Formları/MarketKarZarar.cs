9478798acee199812bf12d3564b79675ac6141eb
Alper Özkul <alper.ozkul@bil.omu.edu.tr>
2023-01-17 01:24:46 +0300

    Kod düzenlemesi yapıldı.

--- Raporlama Formları/MarketKarZarar.cs
+++ Raporlama Formları/MarketKarZarar.cs
@@ -22,63 +22,34 @@ namespace MarketManagementSystem
             try
             {
                 var toplamsatıs = db.Sepets.ToList().Sum(x => x.toplamTutar);
-                label6.Text = toplamsatıs + " ₺".ToString();
+                label6.Text = toplamsatıs + " ₺";
                 label6.ForeColor = Color.Green;
 
                 var tedarikcitoplam = db.Tedarikcis.ToList().Sum(t => t.tedarikciBorc);
-                label7.Text = tedarikcitoplam + " ₺".ToString();
+                label7.Text = tedarikcitoplam + " ₺";
                 label7.ForeColor = Color.Blue;
 
                 var gorekar = toplamsatıs - tedarikcitoplam; 
-                label8.Text =  gorekar + " ₺".ToString();
-                if (gorekar > 0)
-                {
-                    label8.ForeColor = Color.Green;
-                }
-                else
-                {
-                    label8.ForeColor = Color.Red;
-                }
+                label8.Text =  gorekar + " ₺";
+
+                if(gorekar > 0) label8.ForeColor = Color.Green;
+                else label8.ForeColor = Color.Red;
 
                 var musteriBorc = db.Musteris.ToList().Sum(m => m.borcMiktar);
-                label9.Text =  musteriBorc + " ₺".ToString();
+                label9.Text =  musteriBorc + " ₺";
                 label9.ForeColor = Color.Blue;
 
                 var sonuc = toplamsatıs - musteriBorc - tedarikcitoplam;
-                label10.Text =  sonuc + " ₺".ToString();
-                if (sonuc > 0)
-                {
-                    label10.ForeColor = Color.Green;
-                }
-                else
-                {
-                    label10.ForeColor = Color.Red;
-                }
+                label10.Text =  sonuc + " ₺";
+
+                if (sonuc > 0) label10.ForeColor = Color.Green;
+                else label10.ForeColor = Color.Red;
+
             }
             catch
             {
                 MessageBox.Show("Lütfen kontrol edin");
             }
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
 
         }
     }
